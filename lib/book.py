4729d56a2440089ff8bffaedbbb4474536b0551d
Tristan Tritch <ttmeep101@hotmail.com>
2024-12-05 19:02:43 -0600

    ADD: completed tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+class Book():
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        return "Flipping the page...wow, you read fast!\n"
+
+test = Book('title', 100)
+print(test)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        return self.condition
+    
+    @condition.setter
+    def condition(self, new_condition):
+        self.condition = new_condition
+
+    def cobble(self):
+        self.condition = 'New'
+        return "Your shoe is as good as new!\n"
